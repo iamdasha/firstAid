f244ff76afdffafc7936c47306ce1c66671e44d5
iamdasha <daria.g2424@gmail.com>
2022-12-27 19:45:23 +0300

    Create prototype_17.jsx

---
+++ app/javascript/packs/prototype_17.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_17/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_17')
+  ReactDOM.render(<Container />, container)
+})
