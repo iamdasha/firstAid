2639fcbe394eb4bbfdc12a250b80f610d37b398e
iamdasha <daria.g2424@gmail.com>
2022-12-27 19:45:28 +0300

    Create prototype_19.jsx

---
+++ app/javascript/packs/prototype_19.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_19/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_19')
+  ReactDOM.render(<Container />, container)
+})
